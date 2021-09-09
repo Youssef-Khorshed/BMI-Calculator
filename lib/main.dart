c1ee73e9029023c24e458dd7ade85b22128b5017
youssef <youssefmohamedelsayed2000@gmail.com>
2021-09-09 03:50:08 +0200

    hello

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:bm/my_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.back
+      ),
+      home: Home(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
